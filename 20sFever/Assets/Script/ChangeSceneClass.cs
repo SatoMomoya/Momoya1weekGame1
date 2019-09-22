9285b588abeaecbb7dc3c644ba148e74ac6f85e6
SatoMomoya <goodragon2158@gmail.com>
2019-09-22 23:25:23 +0900

    セレクトシーンを追加 シーンを変更する関数を追加 シーン遷移の仕様を決定

---
+++ 20sFever/Assets/Script/ChangeSceneClass.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+/// 
+///シーン変更用クラス
+public class ChangeSceneClass : MonoBehaviour
+{
+    //登録しているシーン
+    public  enum SceneName
+    {
+        LOGO_SCENE  = 0,//ロゴシーン
+        TITLE_SCENE = 1,//タイトルシーン
+        SELECT_SCENE= 2,//セレクトシーン
+        PLAY_SCENE =  3,//プレイシーン
+   
+        NUM
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    /// <summary>
+    /// シーンを切り替える関数
+    /// </summary>
+    /// <param name="changeSceneName">切り替えるシーンの名前</param>
+public static void ChangeScene(SceneName changeSceneName)
+       {
+          SceneManager.LoadSceneAsync((int)changeSceneName);
+       }
+}
---
+++ 20sFever/Assets/Script/ChangeToTitleScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+///タイトルシーンへ切り替えるクラス
+public class ChangeToTitleScene : MonoBehaviour
+{
+    //シーンを切り替える時間
+    public const float maxTime = 1.0f;
+    //現在のカウントしている時間
+    private float nowCountTime = 0.0f;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //経過時間を足し続ける
+        nowCountTime += Time.deltaTime;
+        //もしnowCountTimeがmaxTimeを超えたら
+        //シーンをタイトルシーンに切り替える
+        if (nowCountTime > maxTime)
+        {
+            ChangeSceneClass.ChangeScene(ChangeSceneClass.SceneName.TITLE_SCENE);
+        }
+    }
+}
